245861ce63777d348b89ccdbcd9341827fa7da5a
John Runyard <jrunyard3ft@gmail.com>
2024-12-02 13:04:41 -0600

    Starting on Day 2!

---
+++ 2/part1.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const stdinBuffer = fs.readFileSync(process.stdin.fd);
+const input = stdinBuffer.toString();
+const rows = input.split("\n");
+
+const safe_reports = rows.reduce((accumulator, row) => {
+  if (row === "") return accumulator; // Empty row check, skip
+
+  const report = row.split(" ").map(Number);
+  let direction = true; // ascending
+  let valid = true;
+
+  for (let i = 0; i < report.length - 1; i++) {
+    // Easy case, check for no change, short circuit if so
+    if (report[i] == report[i + 1]) {
+      valid = false;
+      break;
+    }
+
+    if (i == 0) {
+      // Define expected direction
+      direction = report[i] < report[i + 1];
+    }
+
+    // Test!
+    valid = direction
+      ? report[i] < report[i + 1] && report[i + 1] - report[i] < 4 // Ascending and diff of < 4
+      : report[i] > report[i + 1] && report[i] - report[i + 1] < 4; // Descending and diff of < 4
+
+    if (!valid) {
+      // short circuit if we found an exception
+      break;
+    }
+  }
+
+  return accumulator + (valid ? 1 : 0);
+}, 0);
+
+console.log({ safe_reports });
