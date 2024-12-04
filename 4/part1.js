75aa4f19f0da41ee8d1d2594e10abf7c2fbf8a21
John Runyard <jrunyard3ft@gmail.com>
2024-12-04 16:56:10 -0600

    A good start! Added sample input for testing.

--- 4/part1.js
+++ 4/part1.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 let buffer, input, rows;
 
 try {
-  buffer = fs.readFileSync(__dirname + "/input.txt", "utf8");
+  buffer = fs.readFileSync(__dirname + "/sample.txt", "utf8");
 } catch (e) {
   throw e;
 }
@@ -11,10 +11,59 @@ try {
 input = buffer.toString();
 rows = input.split("\n");
 
-const multiply_sum = rows.reduce((accumulator, row) => {
+// console.log({ rows });
+
+// Go through each row, find X.
+// Pass x to find xmas function
+// Return 1 if found, 0 if not, move on to next X
+
+function find_neighbor(letter, direction, row_index, letter_index) {}
+
+function find_xmas(letter, rows, row_index, letter_index) {
+  if (letter != "X") return 0;
+
+  // Letter is X, check in all directions for M
+  // Store direction, check A, so on till S
+  // Return 1 if found all letters, else 0
+
+  if (
+    rows[row_index][letter_index + 1] &&
+    rows[row_index][letter_index + 1] == "M"
+  ) {
+    console.log("Found XM: ", row_index, letter_index);
+  }
+
+  if (
+    rows[row_index][letter_index - 1] &&
+    rows[row_index][letter_index - 1] == "M"
+  ) {
+    console.log("Found MX: ", row_index, letter_index);
+  }
+
+  if (rows[row_index + 1] && rows[row_index + 1][letter_index] == "M") {
+    console.log("Found X/M: ", row_index, letter_index);
+  }
+
+  if (rows[row_index - 1] && rows[row_index - 1][letter_index] == "M") {
+    console.log("Found M/X: ", row_index, letter_index);
+  }
+
+  // ? ? ?
+  // ? X ?
+  // ? ? ?
+
+  return 1;
+}
+
+const xmass = rows.reduce((accumulator, row, index) => {
   if (row === "") return accumulator; // Empty row check, skip
   let sum = 0;
+
+  row.split("").forEach((letter, letter_index) => {
+    sum += find_xmas(letter, rows, index, letter_index);
+  });
+
   return accumulator + sum;
 }, 0);
 
-console.log({ multiply_sum });
+console.log({ xmass });
