af896155746b0c5b4ea1b521f0344f220b3f4b3f
John Runyard <jrunyard3ft@gmail.com>
2024-12-08 13:07:08 -0600

    Day 4 Complete!

--- 4/part2.js
+++ 4/part2.js
@@ -1,20 +1,84 @@
 const fs = require("fs");
+const filename = "input.txt";
 
-let buffer, input, rows;
+var buffer, input, rows;
 
 try {
-  buffer = fs.readFileSync(__dirname + "/input.txt", "utf8");
+  buffer = fs.readFileSync(__dirname + "/" + filename, "utf8");
 } catch (e) {
   throw e;
 }
 
 input = buffer.toString();
-rows = input.split("\n");
+rows = input.split("\n").map(el => el.split(""));
 
-const multiply_sum = rows.reduce((accumulator, row) => {
-  if (row === "") return accumulator; // Empty row check, skip
-  let sum = 0;
-  return accumulator + sum;
-}, 0);
+function find_xmas(letter, i, j) {
+  let total = 0;
+  if (letter !== 'A') return total; // This aint it fam
 
-console.log({ multiply_sum });
+  // If the coords exists, check for the letter
+  // M ? S
+  // ? A ?
+  // M ? S
+  if (rows[i - 1] && rows[i - 1][j - 1] && rows[i - 1][j - 1] === "M") {
+    if (rows[i - 1] && rows[i - 1][j + 1] && rows[i - 1][j + 1] === "S") {
+      if (rows[i + 1] && rows[i + 1][j - 1] && rows[i + 1][j - 1] === "M") {
+        if (rows[i + 1] && rows[i + 1][j + 1] && rows[i + 1][j + 1] === "S") {
+          total++;
+        }
+      }
+    }
+  }
+
+  // M ? M
+  // ? A ?
+  // S ? S
+  if (rows[i - 1] && rows[i - 1][j - 1] && rows[i - 1][j - 1] === "M") {
+    if (rows[i - 1] && rows[i - 1][j + 1] && rows[i - 1][j + 1] === "M") {
+      if (rows[i + 1] && rows[i + 1][j - 1] && rows[i + 1][j - 1] === "S") {
+        if (rows[i + 1] && rows[i + 1][j + 1] && rows[i + 1][j + 1] === "S") {
+          total++;
+        }
+      }
+    }
+  }
+
+  // S ? S
+  // ? A ?
+  // M ? M
+  if (rows[i - 1] && rows[i - 1][j - 1] && rows[i - 1][j - 1] === "S") {
+    if (rows[i - 1] && rows[i - 1][j + 1] && rows[i - 1][j + 1] === "S") {
+      if (rows[i + 1] && rows[i + 1][j - 1] && rows[i + 1][j - 1] === "M") {
+        if (rows[i + 1] && rows[i + 1][j + 1] && rows[i + 1][j + 1] === "M") {
+          total++;
+        }
+      }
+    }
+  }
+
+  // S ? M
+  // ? A ?
+  // S ? M
+  if (rows[i - 1] && rows[i - 1][j - 1] && rows[i - 1][j - 1] === "S") {
+    if (rows[i - 1] && rows[i - 1][j + 1] && rows[i - 1][j + 1] === "M") {
+      if (rows[i + 1] && rows[i + 1][j - 1] && rows[i + 1][j - 1] === "S") {
+        if (rows[i + 1] && rows[i + 1][j + 1] && rows[i + 1][j + 1] === "M") {
+          total++;
+        }
+      }
+    }
+  }
+
+
+  return total;
+}
+
+let found_xmass = 0;
+
+for (let i = 0; i < rows.length; i++) {
+  for (let j = 0; j < rows[i].length; j++) {
+    found_xmass += find_xmas(rows[i][j], i, j);
+  }
+}
+
+console.log({ found_xmass });
